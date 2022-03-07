3929618ebb948a07ea1a42adb9ae38ff574f05f3
xyr_luoyikun <1806867003@qq.com>
2022-03-07 18:41:55 +0800

    新图集测试

---
+++ Assets/AssetbundleDemo/SpriteAtlasTestAb/SpriteAtlasAbTest.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SpriteAtlasAbTest : MonoBehaviour
+{
+    public Image m_img1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        string assetPath = "AssetbundleDemo/SpriteAtlasTestAb/ShanTu/ShanTu";
+        var assetType = typeof(GameObject);
+        xasset.Asset.LoadAsync(assetPath, assetType, (asset) => {
+            m_img1.sprite = asset.Get<Sprite>();
+
+        });
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
